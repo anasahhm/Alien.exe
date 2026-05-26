de470941b5cdcad048811f8a65a56323daea0002
Anas Ahmed <officialanasahmed2001@gmail.com>
2026-05-26 21:29:21 +0530

    Implement ship movement system and game settings configuration

---
+++ alien_invasion.py
@@ -0,0 +1,25 @@
+import pygame  # type: ignore[import]
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    #initialize game and create a screen object
+    pygame.init()
+    ai_settings = Settings()
+
+    screen=pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien.exe")
+
+    # Create a ship
+    ship = Ship(screen)
+
+    #start the main loop for the game
+    while True:        
+        #watch for keyboard and mouse events
+        gf.check_events(ship)
+        ship.update()
+        gf.update_sceen(ai_settings, screen, ship)
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """A class to store all settings for Alien.exe"""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (128, 128, 128)  # Fun fact - we can create 16 million colors by mixing the RGB values
